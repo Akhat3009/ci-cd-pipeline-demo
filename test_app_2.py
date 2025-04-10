770dbf8c092022a29eb88007f6c7c4c6f433456b
Akhat3009 <alisher.baktygaliyev@gmail.com>
2025-04-10 12:02:18 +0500

    Create test_app_2.py

---
+++ test_app_2.py
@@ -0,0 +1,17 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, CI/CD Pipeline!" in response.data
+
+def test_404_route(client):
+    response = client.get('/nonexistent')
+    assert response.status_code == 404
